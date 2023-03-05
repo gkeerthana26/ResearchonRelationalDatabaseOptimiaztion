2a4eb8ed77d4b0f44a6c9804bdf2a7ae266a4915
Keerthana Gopikrishnan <125687511+gkeerthana26@users.noreply.github.com>
2023-03-05 09:56:10 -0800

    Create concurrent_reads.sh

---
+++ CODES/IPYNB Files/Concurrent reads/Concurrent reads/concurrent_reads.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+python3 script1.py &  
+python3 script2.py & 
+python3 script3.py & 
+python3 script4.py & 
+python3 script5.py &  
+python3 script6.py & 
+python3 script7.py & 
+python3 script8.py & 
+python3 script9.py &  
+python3 script10.py & 
+
