03aad30c53654e4c60d53caefac88adf74dc48b5
Keerthana Gopikrishnan <125687511+gkeerthana26@users.noreply.github.com>
2023-03-05 09:57:11 -0800

    Create Load_local_with_partition.sql

---
+++ CODES/SQL_FILES/Load_local_with_partition.sql
@@ -0,0 +1,225 @@
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-000.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-001.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-002.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-003.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-004.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-005.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-006.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-007.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-008.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-009.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-010.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-011.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-012.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-013.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-014.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-015.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-016.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-017.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-018.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-019.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-020.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-021.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-022.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-023.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+
+
+
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-024.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-025.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-026.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-027.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-028.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-029.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-030.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-031.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-032.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+
+LOAD DATA LOCAL INFILE "D:/DATA225/Project/DATA/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography-033.csv"
+INTO TABLE cdc_covid_data_local
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(case_month,res_state,state_fips_code,res_county,county_fips_code,age_group,sex,race,ethnicity,case_positive_specimen_interval,case_onset_interval,
+process,exposure_yn,current_status,symptom_status,hosp_yn,icu_yn,death_yn,underlying_conditions_yn);
+
